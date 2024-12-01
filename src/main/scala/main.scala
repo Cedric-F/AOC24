fb967a860f75172ba04058c0f505d19423ad94bf
Cedric-F <cedric.fontaine@gmx.fr>
2024-12-01 16:44:54 +0100

    Day 1

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -1,7 +1,7 @@
 package algo.monsieur.aoc
 
 import y24.Day_1
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 @main
 def main(): Unit =
   Day_1.compute()
