cfe20ae24a29afe49cba15aba7166e7fe6e7150d
Cedric Fontaine <cedric.fontaine@ensam.eu>
2024-12-04 18:19:42 +0100

    Day 4

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -6,4 +6,5 @@ import y24.*
 def main(): Unit =
   //Day_1.compute()
   //Day_2.compute()
-  Day_3.compute()
+  //Day_3.compute()
+  Day_4.compute()
--- src/test/scala/y24/TestDay4.scala
+++ src/test/scala/y24/TestDay4.scala
@@ -7,15 +7,16 @@ import org.scalatest.funsuite.AnyFunSuite
 
 class TestDay4 extends AnyFunSuite {
 
-  private val input: Array[String] = "".split("\n")
+  private val sample1: Array[String] = "MMMSXXMASM\nMSAMXMSMSA\nAMXSXMAAMM\nMSAMASMSMX\nXMASAMXAMM\nXXAMMXXAMA\nSMSMSASXSS\nSAXAMASAAA\nMAMMMXMMMM\nMXMXAXMASX".split("\n")
+  private val sample2: Array[String] = ".M.S......\n..A..MSMS.\n.M.S.MAA..\n..A.ASMSM.\n.M.S.M....\n..........\nS.S.S.S.S.\n.A.A.A.A..\nM.M.M.M.M.\n..........".split("\n")
 
-  private val EXPECTED_RESULT_PART_1: Int = 0
-  private val EXPECTED_RESULT_PART_2: Int = 0
+  private val EXPECTED_RESULT_PART_1: Int = 18
+  private val EXPECTED_RESULT_PART_2: Int = 9
 
   test("Part 1 sample result is OK") {
-    assert(part1(input) == EXPECTED_RESULT_PART_1)
+    assert(part1(sample1) == EXPECTED_RESULT_PART_1)
   }
   test("Part 2 sample result is OK") {
-    assert(part2(input) == EXPECTED_RESULT_PART_2)
+    assert(part2(sample2) == EXPECTED_RESULT_PART_2)
   }
 }
